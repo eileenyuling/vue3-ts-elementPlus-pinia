ff6ac2a68f25ed893ebd99494718c3b4d5b84b23
yuling <ling.yu@irtone.com>
2022-11-19 23:52:11 +0800

    feat: 首页布局

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import './style.css'
-import App from './App.vue'
+import 'normalize.css/normalize.css'
+import './styles/index.scss'
 
-createApp(App)
-.use(ElementPlus)
-.use(router)
-.use(createPinia())
-.mount('#app')
+createApp(App).use(ElementPlus).use(router).use(createPinia()).mount('#app')
