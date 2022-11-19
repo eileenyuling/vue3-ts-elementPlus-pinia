5b9d1ea2a8d1008858e22cc32979ea42fff40991
yuling <ling.yu@irtone.com>
2022-11-19 23:23:13 +0800

    feat: 项目流程搭建

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import router from './router/index'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './style.css'
+import App from './App.vue'
+
+createApp(App)
+.use(ElementPlus)
+.use(router)
+.use(createPinia())
+.mount('#app')
